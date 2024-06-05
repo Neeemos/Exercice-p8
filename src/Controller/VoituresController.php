4012a317d53b0b5ce38af31ff6b95cc14dc09623
Neeemos <enzo.belfi@galaxy-hotels.fr>
2024-06-05 14:47:24 +0200

    Ajout de la suppression de voiture

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\VoitureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class VoituresController extends AbstractController
 {
-    #[Route('/', name: 'app_voitures')]
+    #[Route('/', name: 'app_home')]
     public function index(VoitureRepository $repository): Response
     {
         $cars = $repository->findAll();
@@ -19,7 +20,7 @@ class VoituresController extends AbstractController
             'cars' => $cars,
         ]);
     }
-    #[Route('/car/{id}', name: 'app_voiture')]
+    #[Route('/car/{id}', name: 'app_car')]
     public function car(int $id, VoitureRepository $repository): Response
     {
         $car = $repository->find($id);
@@ -30,5 +31,16 @@ class VoituresController extends AbstractController
             'controller_name' => 'VoituresController',
             'car' => $car,
         ]);
+    }
+    #[Route('/car/{id}/remove', name: 'app_car_remove')]
+    public function remove(int $id, VoitureRepository $repository,  EntityManagerInterface $entityManagerInterface): Response
+    {
+        $car = $repository->find($id);
+        if (!$car) {
+            return $this->redirect('/');
+        }
+        $entityManagerInterface->remove($car);
+        $entityManagerInterface->flush();
+        return $this->redirect('/');
     }
 }
